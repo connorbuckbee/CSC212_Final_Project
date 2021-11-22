d017ebd4f51d7460cbbc8e6640b7994603d80cd3
Connor <connorbuckbee@uri.edu>
2021-11-21 19:15:37 -0500

    Update btree.h

--- btree.h
+++ btree.h
@@ -2,37 +2,33 @@
 #define BTREE_H
 
 #include <string>
+#include <fstream>
 #include "node.h"
 
 class BTree {
-	    
-	Node *root;     // pointer to root node
-	int degree;     // minimum degree
+        
+        Node *root;     // pointer to root node
+        int degree;     // minimum degree
 
     public:
 
-	// BTree constructor 
-        BTree(int degree) {
-		root = nullptr;  
-            	this->degree = degree;
-	}
+        // BTree constructor
+        BTree(int degree);
+ 
+        // main insert function 
+        void insert(std::string key);
+ 
+        // traverse and display the tree
+        void display();
 
-	// main insert function
-	void insert(std::string key);
-	    
-	// if root is not nullptr, then perform traversal		
-        void traverse() {
-		if (root != nullptr) root->traverse();
-	}
-		
-	// condition ? result1 : result2
-	// check if root is nullptr, else search for key
-	Node* search(std::string key) {
-		return (root == nullptr) ? nullptr : root->search(key);
-	}
-	
-	// read in text file for insertion
-        std::vector<std::string> inputArr(std::string filename);
+        // condition ? result1 : result2
+        // check if root is nullptr, else search for key
+        Node* search(std::string key) {
+            return (root == nullptr) ? nullptr : root->search(key);
+        }
+        
+        // read in text file for insertion
+        std::vector<std::string> readInString(std::string filename);
 
 };
 
