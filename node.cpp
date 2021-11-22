dd193fd6f38db0964e5e9922db4afa6d16d42149
Connor <connorbuckbee@uri.edu>
2021-11-21 20:13:27 -0500

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -1,168 +1,186 @@
 #include "node.h"
 
+// Node constructor, initialize with max children and 
+// whether or not this is a leaf Node
 Node::Node(int degree, bool leaf) {
-
 	this->degree = degree;
 	this->leaf = leaf;
-	
-	
-	// 11/16 - the following code solves the vector out-of-bounds error, but i think the insertion
-	// function is still broken somewhere. See btree.cpp for details.
-	
-	for (int i = 0; i < (2 * degree - 1); i++) {
-		this->keyVec.push_back("key" + i);
-	}
-
-	for (int i = 0; i < (2 * degree); i++) {
-		Node* child = new Node(this->degree, this->leaf); 
-		this->childVec.push_back(child);
-	}
-	//*****************************************************************************
-	// 11/9:
-	// this whole block below is used if we were to use arrays for storage
-	// not needed since we are using vectors, but i dont know if we need 
-	// to replace these commands with other ones
-        //*****************************************************************************
-	// 11/11:
-	// something like this instead?
-	// vector<Type> *vect = new vector<Type>;
-        //*****************************************************************************
-	
-	// allocate memory for maximum number of possible keys and child pointers
-        // root has 2 minimum children, all nodes have max (degree - 1) keys
-	//keyVec = new std::string [2 * degree - 1]; 
-    
-        // root has 2 minimum children, all nodes have max (degree) children 
-	//childVec = new Node* [2 * degree];
-	//*****************************************************************************
-
-	// initialize keys as 0
-	numKeys = 0;
 }
 
+// insert a new key in given Node
+// if child of this node is split, we have to insert *key
+// into keyVec and newNode pointer into childVec
+void Node::insert(std::string newKey, std::string* key, Node*& newNode) {
+ 
+    // non-leaf Node
+    if (leaf == false) {
+        int i = 0;
+ 
+        // find first key greater than *key
+        while (i < keyVec.size() && newKey > keyVec.at(i))
+            i++;
+ 
+        // insert newKey into left child of Node with index i
+        childVec.at(i)->insert(newKey, key, newNode);
+ 
+        // no split needed
+        if (newNode == nullptr) return;
+
+	// we have enoung space child Nodes    
+        if (keyVec.size() < 2 * degree - 1) {
+ 
+            // ...so this Node can accommodate a new key and child pointer entry
+            // insert *key into key vector
+            keyVec.insert(keyVec.begin() + i, *key);
+ 
+            // insert newNode into childVec
+            childVec.insert(childVec.begin() + i + 1, newNode);
+ 
+            // this Node was not split, so set newNode to nullptr
+            newNode = nullptr;
+        }
+
+	// not enough space
+        else {
+ 
+            // current Node now has (2 * degree) keys...
+            keyVec.insert(keyVec.begin() + i, *key);
+            childVec.insert(childVec.begin() + i + 1, newNode);
+ 
+            // ...so split it
+            split(key, newNode);
+        }
+    }
+	
+    // leaf Node	
+    else {
+ 	
+        // insert newKey in this node
+        std::vector<std::string>::iterator it;
+ 
+        // find correct position
+	// need to explain this better <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        it = lower_bound(keyVec.begin(), keyVec.end(), newKey);
+ 
+        // insert in correct position
+        keyVec.insert(it, newKey);
+ 
+        // Node is full of keys
+        if (keyVec.size() == 2 * degree) {
+ 
+            // create new Node
+            newNode = new Node(degree, true);
+ 
+            // set (degree + 1) key as parent
+            *key = this->keyVec[degree];
+ 
+            // insert last (degree - 1) keys into new Node
+            for (int i = degree + 1; i < 2 * degree; i++) {
+                newNode->keyVec.push_back(this->keyVec.at(i));
+            }
+ 
+            // this->Node stores first (degree) keys
+            this->keyVec.resize(degree);
+        }
+    }
+}
 
 // utility function to split the child of this node
-// note that fullNode must be full when this function is called
-void Node::splitChild(int childIndex, Node *fullNode) {
-	 
-	// create new node which is going to store (degree-1) keys of fullNode
-	Node *childNode = new Node(fullNode->degree, fullNode->leaf);
-	childNode->numKeys = degree - 1;
-
-	// copy the last (degree -1) keys of fullNode to childNode
-	for (int j = 0; j < degree - 1; j++)
-		childNode->keyVec.at(j) = fullNode->keyVec.at(j + degree);
-
-	// copy the last (degree) children of fullNode to childNode
-		if (fullNode->leaf == false) {
-		for (int j = 0; j < degree; j++)
-			childNode->childVec.at(j) = fullNode->childVec.at(j + degree);
-	}
-
-	// reduce the number of keys in fullNode
-	fullNode->numKeys = degree - 1;
-
-	// create space for new child
-	for (int j = numKeys; j >= childIndex + 1; j--)
-		childVec.at(j + 1) = childVec.at(j);
-
-	// link the new child to this node
-	childVec.at(childIndex + 1) = childNode;
-
-	// key of fullNode will move to this node 
-	// find location of new key and move all greater keys one space ahead
-	for (int j = numKeys - 1; j >= childIndex; j--)
-		keyVec.at(j + 1) = keyVec.at(j);
-
-	// copy the middle key of fullNode to this node
-	keyVec.at(childIndex) = fullNode->keyVec.at(degree - 1); // << this makes it right biased?
-
-	// increment count of keys in this node
-	numKeys = numKeys + 1;
-}
-
-
-// utility function to insert new key in node
-// node must be non-full when this function is called 
-void Node::insertNonFull(std::string key)
+// split the current Node and store the new parent value 
+// in *key, and new child pointer in &newNode
+// called only for splitting non-leaf Nodes
+void Node::split(std::string* key, Node*& newNode)
 {
-	// initialize index as rightmost element
-	int idx = numKeys - 1;
-
-	// if this is a leaf node
-	if (leaf == true)
-	{
-		// finds the location of new key to be inserted
-		// move all greater keys to one place ahead
-		while (idx >= 0 && keyVec.at(idx) > key)
-		{
-			keyVec.at(idx + 1) = keyVec.at(idx);
-			idx--;
-		}
-
-		// insert new key at found location
-		keyVec.at(idx + 1) = key;
-		numKeys = numKeys + 1;
-	}
-	else // this node is not leaf
-	{
-		// find the child which will have the new key
-		while (idx >= 0 && keyVec.at(idx) > key)
-			idx--;
-
-		// check if the found child is full
-		if (childVec.at(idx + 1)->numKeys == 2 * degree - 1)
-		{
-			// if child is full, then split it
-			splitChild(idx + 1, childVec.at(idx + 1));
-
-			// after split, the middle key of childVec[idx] goes up and
-			// childVec[idx] is split in two
-			// see which of the two is going to have the new key
-			if (keyVec.at(idx + 1) < key)
-				idx++;
-		}
-		childVec.at(idx + 1)->insertNonFull(key);
-	}
+ 
+    // create new non-leaf Node
+    newNode = new Node(degree, false);
+ 
+    // (degree + 1)th Node becomes parent
+    *key = this->keyVec.at(degree);
+ 
+    // last (degree - 1) entries will go to newNode
+    for (int i = degree + 1; i < 2 * degree; i++) {
+        newNode->keyVec.push_back(this->keyVec.at(i));
+    }
+ 
+    // this node stores first (degree) entries
+    this->keyVec.resize(degree);
+ 
+    // last (degree) entries will go to newNode
+    for (int i = degree + 1; i <= 2 * degree; i++) {
+        newNode->childVec.push_back(this->childVec.at(i));
+    }
+ 
+    // this Node stores first (degree + 1) entries
+    this->childVec.resize(degree + 1);
 }
 
-
-// traverse all nodes in a subtree rooted with this node
-void Node::traverse() {
-
-	//traverse through n keys and first n children
-	int j;
-	for (j = 0; j < numKeys; j++) {
-		
-		// if this is not leaf, then before printing keyVec.at(j),
-		// traverse the subtree rooted with child childVec.at(j)
-		if (leaf == false)
-			childVec.at(j)->traverse();
-			std::cout << " " << keyVec.at(j);
-	}
-
-	// print the subtree rooted with last child
-	if (leaf == false)
-		childVec.at(j)->traverse();
+// function to create a new root
+// set current Node as its child
+Node* Node::makeNewRoot(std::string key, Node* newNode)
+{
+    // create new root
+    Node* root = new Node(degree, false);
+ 
+    // store key value in keyVec
+    root->keyVec.push_back(key);
+ 
+    // push child pointers to childVec
+    root->childVec.push_back(this);
+    root->childVec.push_back(newNode);
+    return root;
 }
 
+// traverse and print Nodes of BTree
+void Node::traverse(int numNodes)
+{
+    int i;
+    std::string nodeKey;
+ 
+    // print 'numNodes' number of Nodes
+    for (int j = 0; j < numNodes; j++) {
+        nodeKey += '\t';
+    }
+	
+    for (i = 0; i < keyVec.size(); i++) {
 
-// function to search for key in subtree rooted with this node
+        // if this is not a leaf, then before printing keyVec.at(i)
+        // traverse the subtree rooted with childVec.at(i)
+        if (leaf == false)
+            childVec.at(i)->traverse(numNodes + 1);
+
+        std::cout << nodeKey << keyVec.at(i) << std::endl;
+    }
+ 
+    // recursively go to the next child Node
+    if (leaf == false) {
+        childVec.at(i)->traverse(numNodes + 1);
+    }
+}
+
+// function to search for key in subtree rooted with this Node
+// return true if key is present in BTree
 Node *Node::search(std::string key) {
 
-	// find first key greater than or equal to key
-	int j = 0;
-	while (j < numKeys && key > keyVec.at(j))
-		j++;
+    int i = 0;
+    int numKeys = 0;
 
-	// if found key is equal to key, return this node
-	if (keyVec.at(j) == key)
+    // find number of keys in current Node
+    for(int i = 0; i < keyVec.size(); i++) 
+	numKeys++;	
+
+    // find the first key greater than or equal to key 
+    while (i < numKeys && key > keyVec[i])
+        i++;
+
+    // if the found key is equal to key, return this Node
+    if (keyVec[i] == key)
 		return this;
 
-	// if key is not found here and this is a leaf node
-	if (leaf == true)
-		return nullptr;
+    // if the key is not found here and this is a leaf node
+    if (leaf == true)
+        return nullptr;
 
-	// recursively check other child nodes
-	return childVec.at(j)->search(key);
+    // recursively search child Nodes
+    return childVec.at(i)->search(key);
 }
